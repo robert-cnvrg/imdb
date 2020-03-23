c777439c27503ea5fd52022791d46ca8c245b503
robert-cnvrg <60426409+robert-cnvrg@users.noreply.github.com>
2020-03-23 19:09:37 +0200

    Added experiment simulator

---
+++ simulate.py
@@ -0,0 +1,16 @@
+import os
+import time
+
+file_prefix = "output/sim"
+file_index = 1
+
+file_size = 1024*1024*1024
+
+while(True):
+  file_name = file_prefix + str(file_index)
+  with open(file_name, 'wb') as fout:
+    fout.write(os.urandom(file_size))
+    
+  print("Wrote file: " + file_name)
+  time.sleep(60)
+  

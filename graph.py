aa2017dbcd59f9bc378d2ea807e65fb4526afec4
robert-cnvrg <60426409+robert-cnvrg@users.noreply.github.com>
2020-03-24 20:10:52 +0200

    Update graph.py

--- graph.py
+++ graph.py
@@ -4,14 +4,14 @@ import random
 key = "epoch 1"
 while(True):
     val = random.randint(0,9)    
-    print f"cnvrg_linechart_Option1 value: {val}"
+    print(f"cnvrg_linechart_Option1 value: {val}")
     
-    print f"cnvrg_linechart_Option2 key: {key} value: {val}"
+    print(f"cnvrg_linechart_Option2 key: {key} value: {val}")
 
-    print f"cnvrg_linechart_Option3 group: 1 value: {val}"
-    print f"cnvrg_linechart_Option3 group: 2 value: {val + 1}"
+    print(f"cnvrg_linechart_Option3 group: 1 value: {val}")
+    print(f"cnvrg_linechart_Option3 group: 2 value: {val + 1}")
     
-    print f"cnvrg_linechart_Option4 group: 1 key: {key} value: {val}"
-    print f"cnvrg_linechart_Option4 group: 2 key: {key} value: {val + 1}"
+    print(f"cnvrg_linechart_Option4 group: 1 key: {key} value: {val}")
+    print(f"cnvrg_linechart_Option4 group: 2 key: {key} value: {val + 1}")
     
     time.sleep(10)

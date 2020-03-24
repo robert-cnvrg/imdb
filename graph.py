2dae3dfeec7640594d7628642c6b98385e303894
robert-cnvrg <60426409+robert-cnvrg@users.noreply.github.com>
2020-03-24 19:08:34 +0200

    Added graph test script

---
+++ graph.py
@@ -0,0 +1,17 @@
+import time
+import random
+
+key = "epoch 1"
+while(True):
+    val = random.randint(0,9)    
+    print f"cnvrg_linechart_Option1 value: {val}"
+    
+    print f"cnvrg_linechart_Option2 key: {key} value: {val}"
+
+    print f"cnvrg_linechart_Option3 group: 1 value: {val}"
+    print f"cnvrg_linechart_Option3 group: 2 value: {val + 1}"
+    
+    print f"cnvrg_linechart_Option4 group: 1 key: {key} value: {val}"
+    print f"cnvrg_linechart_Option4 group: 2 key: {key} value: {val + 1}"
+    
+    time.sleep(10)

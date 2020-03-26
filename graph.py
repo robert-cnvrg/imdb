d2bc8668432d9f036e63df17c36d0a51deac7eaa
robert-cnvrg <60426409+robert-cnvrg@users.noreply.github.com>
2020-03-26 14:44:35 +0200

    Update graph.py

--- graph.py
+++ graph.py
@@ -8,10 +8,10 @@ while(True):
     
     print(f"cnvrg_linechart_Option2 key: {key} value: {val}")
 
-    print(f"cnvrg_linechart_Option3 group: 1 value: {val}")
-    print(f"cnvrg_linechart_Option3 group: 2 value: {val + 1}")
+    print(f"cnvrg_linechart_Option3 group: '1' value: {val}")
+    print(f"cnvrg_linechart_Option3 x: 'TX' y: 'TY' group: '2' value: {val + 1}")
     
-    print(f"cnvrg_linechart_Option4 group: 1 key: {key} value: {val}")
-    print(f"cnvrg_linechart_Option4 group: 2 key: {key} value: {val + 1}")
+    print(f"cnvrg_linechart_Option4 group: '1' key: '{key}' value: {val}")
+    print(f"cnvrg_linechart_Option4 group: '2' key: '{key}' value: {val + 1}")
     
     time.sleep(10)

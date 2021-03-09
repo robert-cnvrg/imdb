f5d65614e53591dd4556f540050f6fd75ce0b5d1
robert-cnvrg <60426409+robert-cnvrg@users.noreply.github.com>
2021-03-09 18:00:02 +0200

    Create files.py

---
+++ files.py
@@ -0,0 +1,17 @@
+import os
+import time
+from cnvrg import Experiment
+os.system("mkdir -p testfiles")
+e = Experiment()
+for commit in range(5):
+    file_list = []
+    for i in range(25):
+        with open(f"testfiles/file{commit}{i}.txt", 'w+') as file:
+            file.write('hello')
+        with open(f"testfiles/file{commit}{i}.txt_tags.yml", 'w+') as yml:
+            yml.write(f"---\nid: \"{i}\"\nsource: \"yann lecun\"")
+        file_list.append(f"testfiles/file{commit}{i}.txt")
+        file_list.append(f"testfiles/file{commit}{i}.txt_tags.yml")
+    e.log_artifacts(file_list)
+    time.sleep(5)
+    print(f"commited: {commit}")
